72b316ca19698bc9816f443ecc5b20dd925c2cd9
Bhabani <bhabanism@outlook.com>
2017-04-09 21:14:08 +0530

    reading csv

---
+++ R1.R
@@ -0,0 +1,10 @@
+# Style guide : https://google.github.io/styleguide/Rguide.xml
+# p <- "Hello World1"
+# print(p)
+x <- 1:5
+y <- c(6,7,8,9,10)
+x + y
+x * 2
+ls()
+sn.csv <- read.csv("data.csv")
+sn.csv
